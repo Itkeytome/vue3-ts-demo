b5897811ef73004d47833cb44d0a39a12e01d2e0
陈适时 <csw1392153909@163.com>
2023-04-23 22:59:48 +0800

    style: 调整目录结构

---
+++ src/views/progress/ProgressView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="progress">
+    <div class="progress-main"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.progress {
+  width: 100%;
+  background-color: darkblue;
+
+  &-main {
+    height: 800px;
+  }
+}
+</style>
