360823fdb344c61988db40162757752a76299463
陈适时 <csw1392153909@163.com>
2023-04-23 23:00:51 +0800

    style: 修改引用路径

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ProgressView from '../views/ProgressView.vue'
+import ProgressView from '../views/progress/ProgressView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
